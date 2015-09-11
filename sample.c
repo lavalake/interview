d328a5e019721b67c1b0c632383f92469a1dde62
jason <lavalakewl@gmail.com>
2015-09-11 13:43:49 -0700

    reverse string

---
+++ sample.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdlib.h>
+void string_reverse1(char *string){
+    int len = strlen(string);
+    int left=0,right=len-1;
+    while(left<right){
+        char temp = string[left];
+        string[left]=string[right];
+        string[right]=temp;
+
+    }
+}
+char *string_reverse2(const char *string){
+    int len = strlen(string);
+    char *reverse_string = (char*) malloc(len);
+    if(!reverse_string)
+        return NULL;
+    for(int i=0; i<len; ++i){
+        reverse_string[i] = string[len-i-1];
+    }
+    return reverse_string;
+}
+int main(){
+    char *str1 = "this is test one";
+    char *str2 = NULL;
+    string_reverse1(str1);
+    str2 = string_reverse2(str1);
+    free(str2);
+    return 0;
+}
